9fbfa936aab80177f0b2a71c582fe903eb8fd084
SpongyBacon <spongybacon@hotmail.co.uk>
2016-03-06 22:52:18 +0000

    WordSubtraction

---
+++ src/main/java/wordsubtraction/WordSubtraction.java
@@ -0,0 +1,31 @@
+package wordsubtraction;
+
+import java.util.Scanner;
+
+public class WordSubtraction {
+
+    public WordSubtraction() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a string:");
+        String one = scanner.nextLine();
+        System.out.println("Input another string:");
+        String two = scanner.nextLine();
+        System.out.println("Cheers buddy.");
+
+        int oneValue = 0;
+        for (char c : one.toCharArray()) {
+            oneValue += c;
+        }
+
+        int twoValue = 0;
+        for (char c : two.toCharArray()) {
+            twoValue += c;
+        }
+
+        System.out.println("First value: " + oneValue + "\r\nSecond value: " + twoValue + "\r\nDiff: " + (oneValue - twoValue));
+    }
+
+    public static void main(String[] args) {
+        new WordSubtraction();
+    }
+}
